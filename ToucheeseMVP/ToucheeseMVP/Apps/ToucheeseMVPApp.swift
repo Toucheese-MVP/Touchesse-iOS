9f7a15817fcbb4b9eeaa9b7d09dd04f938af54b7
JURI <80569323+juri123123@users.noreply.github.com>
2025-03-14 18:22:51 +0900

    feat: #61 파이어베이스 설정 완료

--- ToucheeseMVP/ToucheeseMVP/Apps/ToucheeseMVPApp.swift
+++ ToucheeseMVP/ToucheeseMVP/Apps/ToucheeseMVPApp.swift
@@ -13,80 +13,65 @@ import KakaoSDKAuth
 import KakaoSDKUser
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
-        // MARK: - Firebase 관련 설정
-        FirebaseApp.configure()
-        Messaging.messaging().delegate = self
         
-        // MARK: - Push Notification 권한 설정
+        // 파이어베이스 설정
+        FirebaseApp.configure()
+        
+        // 앱 실행 시 사용자에게 알림 허용 권한을 받음
         UNUserNotificationCenter.current().delegate = self
         
-        let center = UNUserNotificationCenter.current()
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
         
-        // 앱에서 알림 권한을 요청하는 메서드
-        center.requestAuthorization(options: authOptions) { granted, error in
-            guard error == nil else {
-                print("Error while requesting permission for notifications.")
-                return
-            }
-            
-            if granted {
-                print("Authorization granted.")
-                DispatchQueue.main.async {
-                    // APNs에 등록
-                    UIApplication.shared.registerForRemoteNotifications()
-                }
-            } else {
-                print("Authorization denied or undetermined.")
-            }
-        }
+        // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
+        application.registerForRemoteNotifications()
+        
+        // 파이어베이스 Meesaging 설정
+        Messaging.messaging().delegate = self
         
         return true
     }
-    
-    // 디바이스가 APNs 등록에 실패했을 때 호출되는 메서드
-    func application(
-        _ application: UIApplication,
-        didFailToRegisterForRemoteNotificationsWithError error: any Error
-    ) {
-        print(error.localizedDescription)
-    }
-    
-    // 디바이스가 APNs 등록에 성공했을 때 호출되는 메서드
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
     func application(
         _ application: UIApplication,
         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
     ) {
-        #if DEBUG
-        let deviceTokenString = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
-        print(deviceTokenString)
-        #endif
-        
-        // APN 토큰을 명시적으로 FCM 등록 토큰에 매핑하는 코드
+        print("APNS token: \(deviceToken)")
         Messaging.messaging().apnsToken = deviceToken
     }
-}
-
-
-extension AppDelegate: UNUserNotificationCenterDelegate { }
-
-
-extension AppDelegate: MessagingDelegate {
-    func messaging(
-        _ messaging: Messaging,
-        didReceiveRegistrationToken fcmToken: String?
+    
+    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
     ) {
-        #if DEBUG
-        let deviceToken:[String: String] = ["token": fcmToken ?? ""]
-        print("Device token: ", deviceToken)
-        #endif
+        completionHandler([.list, .banner])
+    }
+    
+    // 파이어베이스 MessagingDelegate 설정
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
     }
 }
-
 
 @main
 struct ToucheeseMVPApp: App {
@@ -124,18 +109,18 @@ struct ToucheeseMVPApp: App {
                         print("로그아웃 상태")
                     case .authenticated:
                         print("로그인 상태(토큰 갱신 성공)")
-//                        // 예약 정보 불러오기
-//                        await reservationListViewModel.fetchReservations()
-//                        // 이전 예약 정보 불러오기
-//                        await reservationListViewModel.fetchPastReservations()
-//                        // 좋아요 표시한 스튜디오 불러오기
-//                        AuthenticationManager.shared.failedAuthentication()
+                        //                        // 예약 정보 불러오기
+                        //                        await reservationListViewModel.fetchReservations()
+                        //                        // 이전 예약 정보 불러오기
+                        //                        await reservationListViewModel.fetchPastReservations()
+                        //                        // 좋아요 표시한 스튜디오 불러오기
+                        //                        AuthenticationManager.shared.failedAuthentication()
                     }
                 }
         }
     }
 }
-    
+
 //    /// FCM 토큰을 백엔드 서버에 POST 하는 메서드
 //    private func postDeviceTokenRegistrationData() {
 //        Task {
