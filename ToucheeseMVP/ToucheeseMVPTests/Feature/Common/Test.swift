59440c82c2bac1080d83f8d6f7efd3cdd9970e2a
kang <kangsworkspace@gmail.com>
2025-04-17 17:23:33 +0900

    feat: MoneyStringFormat TestCase 추가

---
+++ ToucheeseMVP/ToucheeseMVPTests/Feature/Common/Test.swift
@@ -0,0 +1,24 @@
+//
+//  Test.swift
+//  ToucheeseMVPTests
+//
+//  Created by 강건 on 4/17/25.
+//
+
+import Testing
+@testable import ToucheeseMVP
+
+struct MoneyFormatTests {
+    @Test func testExtension_MoneyStringFormat_MultipleCases() async throws {
+        let testCases: [(input: Int, expected: String)] = [
+            (3000, "3,000원"),
+            (0, "0원"),
+            (-200, "-200원"),
+            (1000000, "1,000,000원"),
+        ]
+        
+        for (input, expected) in testCases {
+            #expect(input.moneyStringFormat == expected, "testExtension_MoneyStringFormat_MultipleCases Failed for input: \(input)")
+        }
+    }
+}
