c39c7637899e9e4cde8c050ac11fbe66c8a92e2c
kang <kangsworkspace@gmail.com>
2025-04-23 00:34:03 +0900

    feat: Calendar 관련 테스트 코드 추가

--- ToucheeseMVP/ToucheeseMVPTests/Feature/CustomCalendar/Tests/CustomCalendarTests.swift
+++ ToucheeseMVP/ToucheeseMVPTests/Feature/CustomCalendar/Tests/CustomCalendarTests.swift
@@ -87,13 +87,75 @@ struct CustomCalendarTests {
         #expect(viewModel.displayTimeString == "03:00")        
     }
     
+    @Test("selectPreviousMonth() 함수로 calendarMonth가 이전 달의 값으로 바뀌는지")
+    func test_selectPreviousMonth() {
+        // Given
+        let viewModel = makeViewModel()
+        let tartgetMonth = Calendar.current.date(byAdding: .month, value: -1, to: viewModel.calendarMonth)
+        
+        // When
+        viewModel.selectPreviousMonth()
+        
+        // Then
+        #expect(viewModel.calendarMonth == tartgetMonth)
+    }
+    
+    @Test("selectNextMonth() 함수로 calendarMonth가 이전 달의 값으로 바뀌는지")
+    func test_selectNextMonth() {
+        // Given
+        let viewModel = makeViewModel()
+        let tartgetMonth = Calendar.current.date(byAdding: .month, value: 1, to: viewModel.calendarMonth)
+        
+        // When
+        viewModel.selectNextMonth()
+        
+        // Then
+        #expect(viewModel.calendarMonth == tartgetMonth)
+    }
+    
+    @Test("confirmSelect() 선택했을 때 리턴하는 값이 사용자가 선택한 날짜와 일치하는지", arguments: DateArray.allCases)
+    func test_confirmSelect(dateCase: DateArray) async {
+        // Given
+        let viewModel = makeViewModel()
+        let targetDate = Calendar.current.date(bySettingHour: 3, minute: 0, second: 0, of: dateCase.date)
+        
+        // When
+        await viewModel.selectDate(date: dateCase.date)
+        await viewModel.selectTime(date: CustomCalendarStub.threeAM)
+
+        // Then
+        #expect(viewModel.confirmSelect() == targetDate)
+    }
+    
+    // (1) 선택한 날짜와 일치했을 때 True를 리턴
+    // (2) 선택한 날짜와 일치하지 않을 때 False를 리턴
+    @Test("isSelectedDate() 함수가 정상적으로 동작하는지", arguments: DateArray.allCases)
+    func test_isSelected(dateCase: DateArray) async {
+        // Given
+        let viewModel = makeViewModel()
+        
+        // (1)
+        // When
+        await viewModel.selectDate(date: dateCase.date)
+        
+        // Then
+        #expect(viewModel.isSelectedDate(dateCase.date))
+        
+        // (2)
+        // When
+        await viewModel.selectDate(date: Date())
+        
+        // Then
+        #expect(!viewModel.isSelectedDate(dateCase.date))
+    }
+    
+    
     /// Test에 사용할 뷰모델을 만드는 함수
     func makeViewModel() -> (any CalendarViewModelProtocol & PrivateCalendarViewModelProtocolLogic) {
         let studioID: Int = 1
         let studioService: StudioService = MockStudioService()
         
-        let viewModel: (any CalendarViewModelProtocol & PrivateCalendarViewModelProtocolLogic) =
-        CustomCalendarViewModel(
+        let viewModel = CustomCalendarViewModel(
             studioID: studioID,
             preSelectedDate: nil,
             studioService: studioService
@@ -102,43 +164,3 @@ struct CustomCalendarTests {
         return viewModel
     }
 }
-
-
-
-
-//    @Test("calReservableTimes2() 함수로 응답값을 통해 제대로 된 예약 날짜 시간이 계산되는지", arguments: DateArray.allCases)
-//    func test_calReservableTimes2(dateCase: DateArray) async {
-//        // Given
-//        let viewModel = makeViewModel()
-//
-//        // When
-//        // fetch를 한번 해줘야 조건을 통과합니다.
-//        // 함수의 동작들이 독립되어 있지 않아 Test 친화적이지 않습니다. 리팩토링이 고려됩니다.
-//        await viewModel.fetchStudioCalendar(dateCase.date)
-//        await viewModel.selectDate(date: dateCase.date)
-//
-//        try? await Task.sleep(nanoseconds: 1_000_000_000)
-//
-//        // Then
-//        #expect(
-//            viewModel.studioReservableTime == (
-//                (
-//                    AM: [
-//                        "11:00",
-//                    ],
-//                    PM: [
-//                        "12:00",
-//                        "14:00",
-//                        "15:00",
-//                        "16:00",
-//                        "17:00",
-//                        "19:00",
-//                        "20:00",
-//                        "21:00",
-//                        "22:00",
-//                    ]
-//                )
-//
-//            )
-//        )
-//    }
